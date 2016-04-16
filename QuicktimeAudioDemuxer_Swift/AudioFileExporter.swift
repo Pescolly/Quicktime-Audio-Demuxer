44af81c070ffe94b6bec475495093b95ac15edef
Armen Karamian <akaramian@tv-met-mac-3.modern.mvfinc.com>
2016-04-15 18:47:48 -0700

    dispatch async is fucking up the loop for stereo pairs.

--- QuicktimeAudioDemuxer_Swift/AudioFileExporter.swift
+++ QuicktimeAudioDemuxer_Swift/AudioFileExporter.swift
@@ -11,13 +11,76 @@ import AVFoundation
 
 class AudioFileExporter
 {
+	//main path where exports go
+	let exportDirectory:NSURL?
+	
+	init(inExportDirectory:NSURL)
+	{
+		self.exportDirectory = inExportDirectory
+	}
+	
 	func exportStereoPair(audioTrack1:AVAssetTrack, audioTrack2:AVAssetTrack, audioTrackName:String)
 	{
-		print("Exporting",audioTrackName)
+		print("Exporting Stereo Pair",audioTrackName)
 	}
 	
 	func exportSingleStem(audioTrack:AVAssetTrack, audioTrackName:String)
 	{
-		print("Exporting",audioTrackName)
+		//setup export path
+		let exportFileName = self.exportDirectory?.URLByAppendingPathComponent(audioTrackName+".wav")
+		
+		//setup export composition and track
+		if let exportComposition = createExportComposition(audioTrack)
+		{
+			//create export session
+			if let exportSession = createExportSession(exportComposition, exportFileName: exportFileName!)
+			{
+				//create semaphore and start export
+				let exportDoneSemaphore = dispatch_semaphore_create(0);
+				exportSession.exportAsynchronouslyWithCompletionHandler(
+					{
+						switch exportSession.status
+						{
+							case AVAssetExportSessionStatus.Completed:
+								print("Export Complete", exportFileName?.path!)
+								dispatch_semaphore_signal(exportDoneSemaphore)
+							case AVAssetExportSessionStatus.Failed:
+								print("Export Failed: Check to see if destination file exists")
+								dispatch_semaphore_signal(exportDoneSemaphore)
+							default:
+								print("Cannot export")
+								dispatch_semaphore_signal(exportDoneSemaphore)
+
+						}
+				})
+				//wait for export to complete
+				dispatch_semaphore_wait(exportDoneSemaphore, DISPATCH_TIME_FOREVER);
+			}
+		}
+		
+	}
+	
+	func createExportSession(exportComposition : AVMutableComposition, exportFileName : NSURL) -> AVAssetExportSession?
+	{
+		let exportSession = AVAssetExportSession(asset: exportComposition, presetName: AVAssetExportPresetPassthrough)
+		exportSession?.outputFileType = AVFileTypeWAVE
+		exportSession?.outputURL = exportFileName
+		return exportSession
+	}
+	
+	func createExportComposition(audioTrack : AVAssetTrack) -> AVMutableComposition?
+	{
+		do
+		{
+			let exportComposition = AVMutableComposition()
+			let exportTrack = exportComposition.addMutableTrackWithMediaType(AVMediaTypeAudio, preferredTrackID: kCMPersistentTrackID_Invalid)
+			try exportTrack.insertTimeRange(audioTrack.timeRange, ofTrack: audioTrack, atTime: kCMTimeZero)
+			return exportComposition
+		}
+		catch
+		{
+			print("Cannot export")
+			return nil
+		}
 	}
 }
--- QuicktimeAudioDemuxer_Swift/main.swift
+++ QuicktimeAudioDemuxer_Swift/main.swift
@@ -9,9 +9,31 @@
 import Foundation
 import AVFoundation
 
+
 //get input info
 let inputFile = Process.arguments[1]
 let inputFileURL = NSURL(fileURLWithPath: inputFile)
+
+//get path of export directory and append new stems folder
+var exportDirectory = inputFileURL.URLByDeletingLastPathComponent
+exportDirectory = exportDirectory?.URLByAppendingPathComponent("AUDIO_STEMS")
+
+//create export path if it does not exist
+let fm = NSFileManager.defaultManager()
+
+if !fm.fileExistsAtPath(exportDirectory!.path!)
+{
+	do
+	{
+		print("creating direcotry at", exportDirectory!.path)
+		try fm.createDirectoryAtURL(exportDirectory!, withIntermediateDirectories: false, attributes: nil)
+	}
+	catch
+	{
+		print("Cannot create directory at ",exportDirectory!.path)
+	}
+}
+
 
 //create asset
 let qtAsset = AVAsset(URL: inputFileURL)
@@ -22,7 +44,11 @@ let qtAudioTracks = qtAsset.tracksWithMediaType(AVMediaTypeAudio)
 //determine number of tracks
 let audioTrackCount = qtAudioTracks.count
 
-let audioFileExporter = AudioFileExporter()
+let audioFileExporter = AudioFileExporter(inExportDirectory: exportDirectory!)
+
+//dispatch group for track export threads
+let trackGroup = dispatch_group_create();
+let trackQ = dispatch_queue_create("com.mvf.trackexportq", DISPATCH_QUEUE_CONCURRENT);
 
 switch audioTrackCount
 {
@@ -30,20 +56,43 @@ switch audioTrackCount
 		audioFileExporter.exportStereoPair(qtAudioTracks[0], audioTrack2: qtAudioTracks[1], audioTrackName: "AudioPairOne")
 	
 	case 4:
-		audioFileExporter.exportStereoPair(qtAudioTracks[0], audioTrack2: qtAudioTracks[1], audioTrackName: "AudioPairOne")
-		audioFileExporter.exportStereoPair(qtAudioTracks[0], audioTrack2: qtAudioTracks[1], audioTrackName: "AudioPairTwo")
+		//assign each track to a async thread in the track export dispatch group.
+		dispatch_group_async(trackGroup, trackQ,
+			{
+				//start async dispatch
+				audioFileExporter.exportStereoPair(qtAudioTracks[0], audioTrack2: qtAudioTracks[1], audioTrackName: "AudioPairOne")
+			})
+
+		dispatch_group_async(trackGroup, trackQ,
+			{
+				//start async dispatch
+				audioFileExporter.exportStereoPair(qtAudioTracks[0], audioTrack2: qtAudioTracks[1], audioTrackName: "AudioPairTwo")
+			})
 	
 	default:
 		for i in 0...5
 		{
-			audioFileExporter.exportSingleStem(qtAudioTracks[i], audioTrackName: String(i+1))
+			dispatch_group_async(trackGroup, trackQ,
+			{
+				audioFileExporter.exportSingleStem(qtAudioTracks[i], audioTrackName: String(i+1))
+			})
 		}
 		
-		for var i=6; i < audioTrackCount-1; i += 2
+		for var index = 6; index <= audioTrackCount-2; index += 2
 		{
-			audioFileExporter.exportStereoPair(qtAudioTracks[i], audioTrack2: qtAudioTracks[i+1], audioTrackName: String(i+1)+"_"+String(i+2))
+			if (index >= audioTrackCount)
+			{
+				break
+			}
+			dispatch_group_async(trackGroup, trackQ,
+			{
+				audioFileExporter.exportStereoPair(qtAudioTracks[index], audioTrack2: qtAudioTracks[index+1], audioTrackName: String(index+1)+"_"+String(index+2))
+			})
 		}
 }
+
+//wait for all async threads to finish
+dispatch_group_wait(trackGroup, DISPATCH_TIME_FOREVER);
 
 //loop thru tracks
 //export on seperate thread
